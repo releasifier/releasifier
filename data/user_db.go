313fae42cee9cb3c6850a0783a5e735b72e7cbe9
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-11-12 17:33:23 -0500

    added user data

--- data/user_db.go
+++ data/user_db.go
@@ -1,1 +1,17 @@
 package data
+
+import "time"
+
+//User struct for storing the basic information about each user
+type User struct {
+	ID       SecureID  `db:"id,omitempty,pk" json:"id"`
+	Fullname string    `db:"fullname" json:"fullname"`
+	Email    string    `db:"email" json:"email"`
+	Password string    `db:"password" json:"password"`
+	CreateAt time.Time `db:"created_at" json:"created_at"`
+}
+
+//CollectionName returns collection name in database
+func (u *User) CollectionName() string {
+	return `users`
+}
