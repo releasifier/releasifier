cf06b3a6f2a0a72b75f1db09eb6ada366e359a40
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-11-25 16:26:07 -0500

    added AppID and Platform to release struct

--- data/release_db.go
+++ data/release_db.go
@@ -5,6 +5,8 @@ import "time"
 //Release structure for data represents in database
 type Release struct {
 	ID       int64      `db:"id,omitempty,pk" json:"id"`
+	AppID    int64      `db:"app_id" json:"app_id"`
+	Platform Platform   `db:"platform" json:"platform"`
 	Note     string     `db:"note" json:"note"`
 	Version  Version    `db:"version" json:"version"`
 	CreateAt *time.Time `db:"created_at" json:"created_at" bondb:",utc"`
